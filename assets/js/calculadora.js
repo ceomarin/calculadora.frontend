7ad3d0da927d36d0350f36f0f70169954564dcb6
ceomarin <cmarin.ing@gmail.com>
2023-04-19 13:11:48 -0400

    cambios en logica

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,41 +1,117 @@
-// selectores
 
-// listener
-document.addEventListener('DOMContentLoaded',()=>{
-    
-})
+let resultado = document.querySelector(".resultado");
+let operacion = "";
 
-// funciones
+let numero1 = [];
+let numero2 = [];
+
+var arrayInputs = [];
+
+
 function sumar(num1, num2) {
-
-    let resultato = 0;
-    resultato = num1 + num2;
-
-    console.log(resultato);
-
+    return num1 + num2;
 }
 function restar(num1, num2) {
 
-    let resultato = 0;
-    resultato = num1 - num2;
-
-    console.log(resultato);
+    return num1 - num2;
 }
 function multiplicar(num1, num2) {
-
-    let resultato = 0;
-    resultato = num1 * num2;
-
-    console.log(resultato);
+    return num1 * num2;
 }
 function dividir(num1, num2) {
 
-    let resultato = 0;
-    resultato = num1 / num2;
-
-    console.log(resultato);
+    return num1 / num2;
 }
 
 function mostarResultado() {
 
 }
+
+function validar(elemento) {
+    let valor = 0;
+    let result = 0;
+    valor = $(elemento).data('value');
+
+    if(valor === "borrar" || valor === "/" || valor === "*"|| valor === "+" || valor === "-"|| valor === "."){
+        console.log(valor);
+        switch (valor) {
+            case "+":
+                operacion = "+";
+                document.querySelector("#sumar").classList.add("disabled");
+                break;
+            case "-":
+                operacion = "-";
+                document.querySelector("#restar").classList.add("disabled");
+            break;
+            case "*":
+                operacion = "*";
+                document.querySelector("#multiplicar").classList.add("disabled");
+            break;
+            case "/":
+                operacion = "/";
+                document.querySelector("#dividir").classList.add("disabled");
+            break;
+            case "borrar":
+                operacion = "borrar";
+                resultado.innerHTML = "0.0";
+                arrayInputs = [];
+                console.log(arrayInputs);
+            break;
+            default:
+                break;
+        }
+
+    }else{
+        numero1.push(validar);
+        console.log(numero1);
+        arrayInputs.push(valor);
+
+        resultado.innerHTML = arrayInputs;
+        console.log(arrayInputs);
+    }
+
+    if(validar === "="){
+        switch (operacion) {
+            case "+":
+                resultado.innerHTML = sumar(arrayInputs[0],arrayInputs[1]);
+                arrayInputs = [];
+                document.querySelector("#sumar").classList.remove("disabled");
+                operacion = "";
+                break;
+            case "-":
+                resultado.innerHTML = restar(arrayInputs[0],arrayInputs[1]);
+                arrayInputs = [];
+                document.querySelector("#restar").classList.remove("disabled");
+                operacion = "";
+            break;
+            case "*":
+                resultado.innerHTML = multiplicar(arrayInputs[0],arrayInputs[1]);
+                arrayInputs = [];
+                document.querySelector("#multiplicar").classList.remove("disabled");
+                operacion = "";
+            break;
+            case "/":
+                resultado.innerHTML = dividir(arrayInputs[0],arrayInputs[1]);
+                arrayInputs = [];
+                document.querySelector("#dividir").classList.remove("disabled");
+                operacion = "";
+            break;
+            case "borrar":
+                resultado.innerHTML = "0.0";
+                arrayInputs = [];
+                console.log(arrayInputs);
+                operacion = "";
+                document.querySelector("#sumar").classList.remove("disabled");
+                document.querySelector("#restar").classList.remove("disabled");
+                document.querySelector("#multiplicar").classList.remove("disabled");
+                document.querySelector("#dividir").classList.remove("disabled");
+
+            break;
+            default:
+                break;
+        }
+    }
+
+
+
+  }
